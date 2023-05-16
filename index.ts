e5579d343821d09903041f5ca088e322281a78ff
SiberianCoffe <siberiancoffe@outlook.cl>
2023-05-16 19:34:59 -0400

    error handling response

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
-//import https from "https";
 import http from "http";
+import { errorHandler } from "./middlewares/errorHandler";
 import routes from "./routes/routes";
 import cors from "cors";
 import { PORT } from "./helpers/config";
@@ -9,9 +9,9 @@ const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 routes(app);
 
-//const httpsServer = https.createServer(credentials, app);
 const httpServer = http.createServer(app);
 
 httpServer.listen(PORT);
